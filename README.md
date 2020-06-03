6128d83497a5e88a2059ba22988ab518184a6703
Amal Soman <amal.soman@C02QD5RNG8WP.domain.name>
2020-06-03 22:06:10 +0530

    script added

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # log-parsing
+## RUN THE SCRIPT AS BELOW
+### If you want to completly parse the file and the access log in available in the same location
+```
+--> python3 main.py
+
+OUTPUT:
+2XX occurrence in time frame: 71591 
+4XX occurrence in time frame: 2030 
+5XX occurrence in time frame: 6778 
+Bytes sent in time frame: 1150283652 
+50th percentile: 6778.0 
+90th percentile: 58628.4 
+95th percentile: 65109.7 
+100th percentile: 71591.0 
+
+```
+### If you have different timeframe to check and you want to pass a path for file 
+```
+---> python3 main.py --help 
+usage: Script to parse the nginx logs [-h] [--starttime ST_TIME]
+                                      [--endtime END_TIME] [--file FILE_PATH]
+
+optional arguments:
+  -h, --help           show this help message and exit
+  --starttime ST_TIME
+  --endtime END_TIME
+  --file FILE_PATH
+
+```
+```
+---> python3 main.py --starttime 27/May/2020:12:37:10 --endtime 27/May/2020:12:39:24 --file access.log 
+
+output
+2XX occurrence in time frame: 362 
+4XX occurrence in time frame: 4 
+5XX occurrence in time frame: 32 
+Bytes sent in time frame: 7069307 
+50th percentile: 32.0 
+90th percentile: 296.0 
+95th percentile: 328.99999999999994 
+100th percentile: 362.0 
+``` 
+
+##Requiremnts
+```
+--->  pip3 freeze
+
+numpy==1.18.4
+```
---
+++ main.py
@@ -0,0 +1,86 @@
+import argparse
+import datetime
+import logging
+import sys
+import numpy as ny
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout, format='%(message)s')
+logger = logging.getLogger()
+
+
+def log_fetch(st_time, end_time, file):
+    stat_dict = dict();
+    stat_dict['2XX'] = 0
+    stat_dict['4XX'] = 0
+    stat_dict['5XX'] = 0
+    stat_dict['UNKNOWN'] = 0
+    stat_dict['Bytesused'] = 0
+    try:
+        with open(file, "r") as logs:
+            for line in logs:
+                date = datetime.datetime.strptime(line.split(" ")[0].split("[")[1], '%d/%b/%Y:%H:%M:%S')
+                if st_time <= date <= end_time:
+                    try:
+                        res = int(line.split(" ")[5])
+                    except(ValueError, TypeError):
+                        continue
+                    if res in range(200, 204):
+                        stat_dict['2XX'] += 1
+                    elif res in range(400, 404):
+                        stat_dict['4XX'] += 1
+                    elif res in range(500, 504):
+                        stat_dict['5XX'] += 1
+                    else:
+                        stat_dict['UNKNOWN'] +=1
+                    byte_read = int(line.split(" ")[6])
+                    stat_dict['Bytesused'] += byte_read
+        return stat_dict
+    except (FileNotFoundError, IOError):
+        logger.info("Error in reading the file, please try again")
+        return 0
+
+
+def percent(arr, per):
+    try:
+        pe = ny.percentile(arr, per)
+        return pe
+    except :
+        logger.info("Error in calculating percentile")
+        return 0
+
+
+def time_check(time):
+    try:
+        r_time = datetime.datetime.strptime(time, '%d/%b/%Y:%H:%M:%S')
+        return r_time
+    except (ValueError, TypeError):
+        logger.info("Please enter the start time and end time in format D/M/Y:H:M:S , Please try again")
+        return 0
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Script to parse the nginx logs")
+    parser.add_argument("--starttime", action="store", dest="st_time", type=str, required=False, default="01/Jan/0001:00:00:00")
+    parser.add_argument("--endtime", action="store", dest="end_time", type=str, required=False, default="31/Dec/9999:00:00:00")
+    parser.add_argument("--file", action="store", dest="file_path", type=str, required=False, default="access.log")
+    args = parser.parse_args()
+    file_path = args.file_path
+    start_time = time_check(args.st_time)
+    en_time = time_check(args.end_time)
+    if en_time <= start_time:
+        logger.info("end time should be larger than start time or issue with the time format")
+        exit(0)
+    stats = log_fetch(start_time, en_time, file_path)
+    if stats == 0:
+        exit(0)
+    else:
+        logger.info("2XX occurrence in time frame: {} \n4XX occurrence in time frame: {} \n"
+                    "5XX occurrence in time frame: {} \nBytes sent in time frame: {} ".format(stats['2XX'], stats['4XX'], stats['5XX'], stats['Bytesused']))
+    stats_arr = []
+    stats_arr.append(stats['2XX'])
+    stats_arr.append(stats['4XX'])
+    stats_arr.append(stats['5XX'])
+    logger.info("50th percentile: {} ".format(percent(stats_arr, 50)))
+    logger.info("90th percentile: {} ".format(percent(stats_arr, 90)))
+    logger.info("95th percentile: {} ".format(percent(stats_arr, 95)))
+    logger.info("100th percentile: {} ".format(percent(stats_arr, 100)))
